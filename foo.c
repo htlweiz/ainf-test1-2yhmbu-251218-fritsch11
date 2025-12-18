0c8617486234cb8810ea1509dcd7e957c4c77817
shrek_fanboy8 <fritsc240110@sr.htlweiz.at>
2025-12-18 12:05:19 +0000

    test1

--- foo.c
+++ foo.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
-
+#include <math.h>
 int main(void)
 {
     printf("%s\n", "Hallo Welt!");
+//Aufgabe 1: Weihnachtskekse
+int number_biscuits = 0;
+
+printf("Gib die Anzahl der Kekse ein \n");
+scanf("%d", number_biscuits);
+
+int diameter_biscuit = 0;
+printf("Gib den Durchmesser der Kekse ein\n");
+scanf("%d", diameter_biscuit);
+
+ int area_biscuit = (diameter_biscuit*diameter_biscuit);
+
+printf("Für die Glasur aller %d Kekse wird Schokolade für ");
+
+
+
+//Aufgabe 2: Christbaumschmuck
+int candles = 0;
+printf("Gib die Anzahl der Kerzen ein \n");
+scanf("%d" , candles);
+
+
 }
